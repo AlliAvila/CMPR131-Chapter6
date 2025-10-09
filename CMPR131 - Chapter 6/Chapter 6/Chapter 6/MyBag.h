e713cd95fa05f26d30b3ff7544473d0fe1d5ef76
AlliAvila <99291013+AlliAvila@users.noreply.github.com>
2025-10-08 20:23:32 -0700

    Update MyBag.h

--- CMPR131 - Chapter 6/Chapter 6/Chapter 6/MyBag.h
+++ CMPR131 - Chapter 6/Chapter 6/Chapter 6/MyBag.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <iostream>
 
 class MyBag
 {
@@ -8,88 +9,129 @@ private:
 	int size;
 	int capacity;
 public:
+	//Constructor
 	MyBag()
 	{
-		bag = nullptr;
+		capacity = 3;
 		size = 0;
-		capacity = 0;
+		bag = new int[capacity];
 	}
+
+	//Destructor
 	~MyBag()
 	{
 		delete[] bag;
 	}
+
+	//Precondition: N/A
+	//Postcondition: Return the current number of elements in the bag
 	int getSize() const
 	{
 		return size;
 	}
+		
+	//Precondition: N/A
+	//Postcondition: Return the current capacity of the bag
+	int getCapacity() const
+	{
+		return capacity;
+	}
+
+	//Precondition: N/A
+	//Postcondition: Insert a new integer into the bag; grow the bag if full
 	void insert(int value)
 	{
-		int* newArray = new int[size + 1];
-		
-		for (int i = 0; i < size; i++)
+		if (size == capacity)
 		{
-			newArray[i] = bag[i];
+			growBag();
 		}
+
+		bag[size] = value;
 
 		size++;
+	}
+
+	//Precondition: N/A
+	//Postcondition: Double the capacity of the bag when full
+	void growBag()
+	{
+		int* temp = new int[capacity * 2];
+
+		capacity *= 2;
+
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = bag[i];
+		}
+
 		delete[] bag;
 
-		bag = newArray;
-		bag[size - 1] = value;
+		bag = temp;
 
 	}
-	void display()
-	{
-		if (size == 0)
-		{
-			string exceptionString = "\n\t\tMyBag is empty.\n\n";
-			throw exceptionString;
-		}
-		else
-		{
-			cout << "\n\t\tMybag contains these sorted integers:";
-			for (int i = 0; i < size; i++)
-				cout << "\n\t\t[" + to_string(i) + "] - " + to_string(bag[i]);
-		}
 
-		cout << "\n";
-	}
+	//Precondition: N/A
+	//Postcondition: Clear all elements from the bag and reset size and capacity
 	void clear()
 	{
-		if (bag == nullptr)
-		{
-			string exceptionString = "\n\t\tMyBag is empty.\n\n";
-			throw exceptionString;
-		}
-
-		delete bag;
+		delete[] bag;
 		size = 0;
-		capacity = 0;
+		capacity = 3;
+		bag = new int[capacity];
+		cout << "\n\t\tMyBag is cleared of all elements.\n";
 	}
+
+	//Precondition: N/A
+	//Postcondition: Sort the elements in the bag in ascending order
 	void sortBag()
 	{
 		sort(bag, bag + size);
 		cout << "\n\tMybag has been sorted.\n\n";
 	}
+
+	//Precondition: index is within the range of 0 to size-1
+	//Postcondition: Remove the element at the specified index and shift remaining elements left; shrink capacity if necessary
 	void remove(int index)
 	{
-		
-		int* TempArray = new int[size - 1];
-		int newIndex = 0;
-
-
-		//copying elements into new array except for deleted one
 		for (int i = 0; i < size; i++)
 		{
-			if (i != index) //this is what skips over the deleted element, will copy everything else into new array
-				TempArray[newIndex++] = bag[i];
+			bag[i] = bag[i++];
+		}
+
+		bag[size - 1] = 0;
+
+		size--;
+
+
+		if (size == (capacity / 2))
+		{
+			shrinkBag();
+
+		}
+
+	}
+
+	//Precondition: N/A
+	//Postcondition: Reduce the capacity of the bag to match the current size
+	void shrinkBag()
+	{
+		capacity = size;
+
+		int* temp = new int[capacity];
+
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = bag[i];
 		}
 
 		delete[] bag;
-		bag = TempArray;
-		size -= 1;  
+
+		bag = temp;
 
 	}
+
+	//Precondition: N/A
+	//Postcondition: Linear search to find the index of a value in the bag; returns -1 if not found
 	int linearSearch(int value)
 	{
 		for (int i = 0; i < size; i++)
@@ -100,10 +142,28 @@ public:
 
 		return -1;
 	}
+
+	//Precondition: index is within the range of 0 to size-1
+	//Postcondition: Overloaded subscript operator to access elements in the bag
 	int operator[](int index) const
 	{
 		return bag[index];
 	}
 
+	//Friend function prototype
+	friend ostream& operator << (ostream& output, const MyBag& bag);
+
 };
 
+//Precondition: N/A
+//Postcondition: Overloaded output operator for MyBag class
+ostream& operator << (ostream& output, const MyBag& bag)
+{
+		output << "\n\t\tMybag contains these sorted integers:";
+		for (int i = 0; i < bag.size; i++)
+			output << "\n\t\t[" + to_string(i) + "] - " + to_string(bag[i]);
+	output << "\n";
+
+	return output;
+
+}
