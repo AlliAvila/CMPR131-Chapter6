cca57dad444e3715eff5893fa4a41dc0de4de716
AlliAvila <99291013+AlliAvila@users.noreply.github.com>
2025-10-08 23:40:24 -0700

    Update MyBag.h

--- CMPR131 - Chapter 6/Chapter 6/Chapter 6/MyBag.h
+++ CMPR131 - Chapter 6/Chapter 6/Chapter 6/MyBag.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <iostream>
 
 class MyBag
 {
@@ -8,6 +9,7 @@ private:
 	int size;
 	int capacity;
 public:
+	//Constructor
 	MyBag()
 	{
 		capacity = 3;
@@ -15,21 +17,28 @@ public:
 		bag = new int[capacity];
 	}
 
+	//Destructor
 	~MyBag()
 	{
 		delete[] bag;
 	}
 
+	//Precondition: N/A
+	//Postcondition: Return the current number of elements in the bag
 	int getSize() const
 	{
 		return size;
 	}
-
+		
+	//Precondition: N/A
+	//Postcondition: Return the current capacity of the bag
 	int getCapacity() const
 	{
 		return capacity;
 	}
 
+	//Precondition: N/A
+	//Postcondition: Insert a new integer into the bag; grow the bag if full
 	void insert(int value)
 	{
 		if (size == capacity)
@@ -42,7 +51,8 @@ public:
 		size++;
 	}
 
-	// changed
+	//Precondition: N/A
+	//Postcondition: Double the capacity of the bag when full
 	void growBag()
 	{
 		int newCapacity;
@@ -68,31 +78,32 @@ public:
 		capacity = newCapacity;
 	}
 
+	//Precondition: N/A
+	//Postcondition: Clear all elements from the bag and reset size and capacity
 	void clear()
 	{
-		if (bag == nullptr)
-		{
-			string exceptionString = "\n\t\tMyBag is empty.\n\n";
-			throw exceptionString;
-		}
-
 		delete[] bag;
-		bag = new int[capacity];
 		size = 0;
 		capacity = 3;
+		bag = new int[capacity];
+		cout << "\n\t\tMyBag is cleared of all elements.\n";
 	}
 
+	//Precondition: N/A
+	//Postcondition: Sort the elements in the bag in ascending order
 	void sortBag()
 	{
 		sort(bag, bag + size);
-		cout << "\n\tMybag has been sorted.\n\n";
+		cout << "\n\tMyBag has been sorted.\n\n";
 	}
 
+	//Precondition: index is within the range of 0 to size-1
+	//Postcondition: Remove the element at the specified index and shift remaining elements left; shrink capacity if necessary
 	void remove(int index)
 	{
-		for (int i = index; i < size - 1; i++)
+		for (int i = 0; i < size; i++)
 		{
-			bag[i] = bag[i + 1];
+			bag[i] = bag[i++];
 		}
 
 		bag[size - 1] = 0;
@@ -108,6 +119,8 @@ public:
 
 	}
 
+	//Precondition: N/A
+	//Postcondition: Reduce the capacity of the bag to match the current size
 	void shrinkBag()
 	{
 		capacity = size;
@@ -125,6 +138,8 @@ public:
 
 	}
 
+	//Precondition: N/A
+	//Postcondition: Linear search to find the index of a value in the bag; returns -1 if not found
 	int linearSearch(int value)
 	{
 		for (int i = 0; i < size; i++)
@@ -136,30 +151,27 @@ public:
 		return -1;
 	}
 
+	//Precondition: index is within the range of 0 to size-1
+	//Postcondition: Overloaded subscript operator to access elements in the bag
 	int operator[](int index) const
 	{
 		return bag[index];
 	}
 
+	//Friend function prototype
 	friend ostream& operator << (ostream& output, const MyBag& bag);
 
 };
 
+//Precondition: N/A
+//Postcondition: Overloaded output operator for MyBag class
 ostream& operator << (ostream& output, const MyBag& bag)
 {
-	if (bag.size == 0)
-	{
-		string exceptionString = "\n\t\tMyBag is empty.\n\n";
-		throw exceptionString;
-	}
-	else
-	{
-		output << "\n\t\tMybag contains these sorted integers:";
+		output << "\n\t\tMyBag contains these sorted integers:";
 		for (int i = 0; i < bag.size; i++)
-			cout << "\n\t\t[" + to_string(i) + "] - " + to_string(bag[i]);
-	}
-
+			output << "\n\t\t[" + to_string(i) + "] - " + to_string(bag[i]);
 	output << "\n";
 
 	return output;
+
 }
